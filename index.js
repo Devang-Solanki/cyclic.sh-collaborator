46ddd98587ccb9f6414e663182583b978439d908
Devang Solanki <75718583+Devang-Solanki@users.noreply.github.com>
2023-11-24 18:13:21 +0530

    Fixed: ReferenceError: Cannot access 'sendToDiscord' before initialization

--- index.js
+++ index.js
@@ -6,31 +6,7 @@ const server = http.createServer(async (req, res) => {
   const discordWebhookURL = process.env.WEBHOOK; // Assuming you've set the environment variable
 
   try {
-    // Store request details
-    const requestMethod = req.method;
-    const requestUrl = req.url;
-    const requestHeaders = Object.entries(req.headers).map(([key, value]) => `${key}: ${value}`).join('\n');
-
-    // Get the client's IP address from x-forwarded-for
-    const xForwardedFor = req.headers['x-forwarded-for'];
-    const remoteIP = xForwardedFor ? xForwardedFor.split(',')[0] : 'Not available';
-
-    let requestBody = '';
-
-    // Capture request body for all request types except GET
-    if (requestMethod !== 'GET') {
-      let bodyChunks = [];
-      req.on('data', (chunk) => {
-        bodyChunks.push(chunk);
-      });
-      req.on('end', () => {
-        requestBody = Buffer.concat(bodyChunks).toString();
-        sendToDiscord(requestMethod, requestUrl, requestHeaders, remoteIP, requestBody);
-      });
-    } else {
-      sendToDiscord(requestMethod, requestUrl, requestHeaders, remoteIP, requestBody);
-    }
-
+    // Function to send request details to Discord
     const sendToDiscord = async (method, url, headers, ip, body) => {
       // Format request details for Discord message
       let discordMessage = `
@@ -69,6 +45,31 @@ ${body}
       res.writeHead(200, { 'Content-Type': 'text/plain' });
       res.end('Request details sent to Discord webhook!');
     };
+
+    // Store request details
+    const requestMethod = req.method;
+    const requestUrl = req.url;
+    const requestHeaders = Object.entries(req.headers).map(([key, value]) => `${key}: ${value}`).join('\n');
+
+    // Get the client's IP address from x-forwarded-for
+    const xForwardedFor = req.headers['x-forwarded-for'];
+    const remoteIP = xForwardedFor ? xForwardedFor.split(',')[0] : 'Not available';
+
+    let requestBody = '';
+
+    // Capture request body for all request types except GET
+    if (requestMethod !== 'GET') {
+      let bodyChunks = [];
+      req.on('data', (chunk) => {
+        bodyChunks.push(chunk);
+      });
+      req.on('end', () => {
+        requestBody = Buffer.concat(bodyChunks).toString();
+        sendToDiscord(requestMethod, requestUrl, requestHeaders, remoteIP, requestBody);
+      });
+    } else {
+      sendToDiscord(requestMethod, requestUrl, requestHeaders, remoteIP, requestBody);
+    }
   } catch (error) {
     res.writeHead(500, { 'Content-Type': 'text/plain' });
     res.end(`Error occurred while sending request details to Discord! ${error}`);
